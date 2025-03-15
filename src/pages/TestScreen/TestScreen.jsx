66e52935543461ba5313e9f7c660fd3479a7c8ca
Harshit Pathak <harshitpathak.hp567@gmail.com>
2025-03-15 22:35:02 +0530

    feat:test screen added

--- src/pages/TestScreen/TestScreen.jsx
+++ src/pages/TestScreen/TestScreen.jsx
@@ -1,14 +1,54 @@
 import React, { useEffect, useState } from "react";
-import "./TestScreen.css"
-import demo from "../../assets/images/Landscape-Color.jpg";
+import "./TestScreen.css";
 import axios from "axios";
 
 const TestScreen = () => {
   const [testData, setTestData] = useState([]);
 
+  const getData = async () => {
+    let data = await axios.get("http://localhost:8000/test-data");
+    setTestData(data.data.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="test-screen">
-        <h1>Test Karao Sajna!!</h1>
+      {testData.map((e) => {
+        return (
+          <div class="main-test-card">
+            <div class="main-test-card-mid">
+              <p>q1 how many days in a leap year</p>
+              <h5>
+                {" "}
+                <button>56</button>
+              </h5>
+              <h5>
+                {" "}
+                <button>56</button>
+              </h5>
+              <h5>
+                {" "}
+                <button>56</button>
+              </h5>
+              <h5>
+                {" "}
+                <button>56</button>
+              </h5>
+            </div>
+            <div class="main-test-card-lower">
+              <h4>
+                <button>pre</button>
+              </h4>
+              <h4>
+                <button>next</button>
+              </h4>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
