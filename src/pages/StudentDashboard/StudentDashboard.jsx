da55da7baac01281b648cd418b73366357e17f29
nan-anomynous <nandinichaturvedi283@gmail.com>
2025-03-17 12:20:24 +0530

    feat:student dashboard updated

--- src/pages/StudentDashboard/StudentDashboard.jsx
+++ src/pages/StudentDashboard/StudentDashboard.jsx
@@ -11,25 +11,30 @@ const StudentDashboard = () => {
         <div className="student-dashboard-main-up-left">
           <img src="card1.jpg" alt="Albart Flores" className="profile-pic" />
           <div className="student-dashboard-main-up-right">
-            <h2>{user.fullname}</h2>
+            {/* <h2>{user.fullname}</h2> */}
+            <h2>bdisbds</h2>
             <div className="student-dashboard-main-up-down-right">
               <p>
                 <strong>Enrollment No</strong> <br />
-                {user.enrollment}
+                {/* {user.enrollment} */}
+                dbakjdsac
               </p>
               <p>
                 <strong>Branch</strong>
                 <br />
-                {user.branch}
+                {/* {user.branch} */}
+                vhgcjhb
               </p>
               <p>
                 <strong>College</strong> <br />
-                {user.college}
+                {/* {user.college} */}
+                nvcfugtvh
               </p>
               <p>
                 <strong>Email</strong>
                 <br />
-                {user.email}
+                {/* {user.email} */}
+                gfdryf
               </p>
             </div>
           </div>
@@ -45,13 +50,15 @@ const StudentDashboard = () => {
             </p>
           </div>
           <div className="student-dashboard-main-mid-card-right">
-            <h4>chchuchuchu</h4>
+            <h4><i class="fa-solid fa-book-open-reader"></i></h4>
           </div>
         </div>
       </div>
       <div className="student-dashboard-main-down">
         <div className="student-dashboard-main-down-up">
           <h2>Test History</h2>
+        </div>
+        <div className="student-dashboard-main-down-lower">
           <table>
             <tr>
               <th>Test</th>
